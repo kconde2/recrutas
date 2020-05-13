df6a5472c976c4569927aa0abf22dce02bca60cc
Kaba CONDE <kabaconde15@gmail.com>
2020-05-13 07:41:04 +0200

    feat(auth): manage non auth and auth pages

---
+++ mobile-app/screens/Auth/SignUpScreen.js
@@ -0,0 +1,12 @@
+import * as React from 'react';
+import { View, Text } from 'react-native';
+
+function SignUpScreen() {
+  return (
+    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+      <Text>Sign Up Screen</Text>
+    </View>
+  );
+}
+
+export default SignUpScreen;
